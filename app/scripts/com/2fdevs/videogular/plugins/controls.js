bb5b9a40bb3abc713fb86025ae627a4da31d6aa3
Raul Jimenez <elecash@gmail.com>
2014-05-30 19:18:33 +0200

    Fixed bug in vgScrubbarcurrenttime, it wasn't updating the current time. Updated demo.

--- app/scripts/com/2fdevs/videogular/plugins/controls.js
+++ app/scripts/com/2fdevs/videogular/plugins/controls.js
@@ -302,6 +302,7 @@ angular.module("com.2fdevs.videogular.plugins.controls", [])
 					function onUpdateTime(target, params){
 						percentTime = Math.round((params[0] / params[1]) * 100);
 						elem.css("width", percentTime + "%");
+						scope.$apply();
 					}
 
 					function onComplete(target, params){
