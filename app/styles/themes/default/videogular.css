f12d598cd73b50ea857072e34d68daf7bddc798d
elecash <elecash@gmail.com>
2015-02-21 18:31:38 +0100

    v1.0.1

--- app/styles/themes/default/videogular.css
+++ app/styles/themes/default/videogular.css
@@ -1,5 +1,5 @@
 /**
- * @license Videogular v1.0.0 http://videogular.com
+ * @license Videogular v1.0.1 http://videogular.com
  * Two Fucking Developers http://twofuckingdevelopers.com
  * License: MIT
  */
