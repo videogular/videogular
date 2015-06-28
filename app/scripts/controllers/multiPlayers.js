2737fb1bc499a468ef127755d69e0519bf706407
elecash <elecash@gmail.com>
2015-06-28 11:31:44 +0200

    Updated demo.

--- app/scripts/controllers/multiPlayers.js
+++ app/scripts/controllers/multiPlayers.js
@@ -3,6 +3,7 @@ angular.module('myApp').controller('MultiPlayersCtrl',
     function ($sce) {
         this.commonConfig = {
             sources: null,
+            loop: true,
             theme: "styles/themes/default/videogular.css",
             plugins: {
                 controls: {
@@ -16,9 +17,9 @@ angular.module('myApp').controller('MultiPlayersCtrl',
         };
 
         this.sources = [
-            {src: $sce.trustAsResourceUrl("assets/videos/videogular.mp4"), type: "video/mp4"},
-            {src: $sce.trustAsResourceUrl("assets/videos/videogular.ogg"), type: "video/ogg"},
-            {src: $sce.trustAsResourceUrl("assets/videos/videogular.webm"), type: "video/webm"}
+            {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
+            {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"},
+            {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"}
         ];
 
         this.videoConfigs = [];
