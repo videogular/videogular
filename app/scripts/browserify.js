abc1510d60e2b6c82e7fd287283ea4f7b20822cb
Raul <elecash@gmail.com>
2015-10-17 17:34:25 +0200

    Removed browserification in this repository, we will add this to the distributable repositories.

---
+++ app/scripts/browserify.js
@@ -0,0 +1,5 @@
+var angular = require('angular');
+
+angular.module('vgBrowserify', [
+    require('videogular')
+]);
--- index.js
+++
@@ -1,5 +0,0 @@
-require('angular');
-require('angular-sanitize');
-require('./dist/videogular/latest/videogular');
-
-module.exports = 'com.2fdevs.videogular';
