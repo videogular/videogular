7f4f3f19150cdea96230de5b610cce7445f984ee
Raul <elecash@gmail.com>
2015-11-15 11:30:19 +0100

    Overlay play now hides itself if we're on a mobile device. This avoids to see two play icons on mobile devices.

--- app/scripts/com/2fdevs/videogular/plugins/vg-overlay-play/vg-overlay-play.js
+++ app/scripts/com/2fdevs/videogular/plugins/vg-overlay-play/vg-overlay-play.js
@@ -22,56 +22,56 @@
 "use strict";
 angular.module("com.2fdevs.videogular.plugins.overlayplay", [])
     .run(
-    ["$templateCache", function ($templateCache) {
-        $templateCache.put("vg-templates/vg-overlay-play",
-            '<div class="overlayPlayContainer" ng-click="onClickOverlayPlay()">\
-              <div class="iconButton" ng-class="overlayPlayIcon"></div>\
-            </div>');
-    }]
-)
-    .directive(
-    "vgOverlayPlay",
-    ["VG_STATES", function (VG_STATES) {
-        return {
-            restrict: "E",
-            require: "^videogular",
-            scope: {},
-            templateUrl: function (elem, attrs) {
-                return attrs.vgTemplate || 'vg-templates/vg-overlay-play';
-            },
-            link: function (scope, elem, attr, API) {
-                scope.onChangeState = function onChangeState(newState) {
-                    switch (newState) {
-                        case VG_STATES.PLAY:
-                            scope.overlayPlayIcon = {};
-                            break;
+        ["$templateCache", function ($templateCache) {
+            $templateCache.put("vg-templates/vg-overlay-play",
+                '<div class="overlayPlayContainer" ng-click="onClickOverlayPlay()" ng-if="!isMobile">\
+                  <div class="iconButton" ng-class="overlayPlayIcon"></div>\
+                </div>');
+        }]
+    )
+    .directive("vgOverlayPlay", ["VG_STATES", "VG_UTILS",
+        function (VG_STATES, VG_UTILS) {
+            return {
+                restrict: "E",
+                require: "^videogular",
+                scope: {},
+                templateUrl: function (elem, attrs) {
+                    return attrs.vgTemplate || 'vg-templates/vg-overlay-play';
+                },
+                link: function (scope, elem, attr, API) {
+                    scope.onChangeState = function onChangeState(newState) {
+                        switch (newState) {
+                            case VG_STATES.PLAY:
+                                scope.overlayPlayIcon = {};
+                                break;
 
-                        case VG_STATES.PAUSE:
-                            scope.overlayPlayIcon = {play: true};
-                            break;
+                            case VG_STATES.PAUSE:
+                                scope.overlayPlayIcon = {play: true};
+                                break;
 
-                        case VG_STATES.STOP:
-                            scope.overlayPlayIcon = {play: true};
-                            break;
-                    }
-                };
+                            case VG_STATES.STOP:
+                                scope.overlayPlayIcon = {play: true};
+                                break;
+                        }
+                    };
 
-                scope.onClickOverlayPlay = function onClickOverlayPlay(event) {
-                    API.playPause();
-                };
+                    scope.onClickOverlayPlay = function onClickOverlayPlay(event) {
+                        API.playPause();
+                    };
 
-                scope.overlayPlayIcon = {play: true};
+                    scope.isMobile = VG_UTILS.isMobileDevice();
+                    scope.overlayPlayIcon = {play: true};
 
-                scope.$watch(
-                    function () {
-                        return API.currentState;
-                    },
-                    function (newVal, oldVal) {
-                        scope.onChangeState(newVal);
-                    }
-                );
+                    scope.$watch(
+                        function () {
+                            return API.currentState;
+                        },
+                        function (newVal, oldVal) {
+                            scope.onChangeState(newVal);
+                        }
+                    );
+                }
             }
         }
-    }
     ]);
 
