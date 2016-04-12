2d90b04f7e63ce751125b51a95576efed786e945
jalexanderfox <jalexanderfox@gmail.com>
2016-04-11 20:39:14 -0400

    add alt attribute to poster img additional accessibility

--- app/scripts/com/2fdevs/videogular/plugins/vg-poster/vg-poster.js
+++ app/scripts/com/2fdevs/videogular/plugins/vg-poster/vg-poster.js
@@ -27,7 +27,7 @@ angular.module("com.2fdevs.videogular.plugins.poster", [])
     .run(
     ["$templateCache", function ($templateCache) {
         $templateCache.put("vg-templates/vg-poster",
-            '<img ng-src="{{vgUrl}}" ng-class="API.currentState" role="presentation">');
+            '<img ng-src="{{vgUrl}}" ng-class="API.currentState" role="presentation" alt="video poster">');
     }]
 )
     .directive("vgPoster",
