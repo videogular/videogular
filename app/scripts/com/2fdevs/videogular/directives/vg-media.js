6aca68a7acbb26425c9aacd00cf561b501e2388b
Tushar Singh <github@tushar.ca>
2016-08-17 18:20:18 -0400

    Updated tag for iOS 10.

--- app/scripts/com/2fdevs/videogular/directives/vg-media.js
+++ app/scripts/com/2fdevs/videogular/directives/vg-media.js
@@ -121,8 +121,13 @@ angular.module("com.2fdevs.videogular")
                         return API.playsInline;
                     },
                     function (newValue, oldValue) {
-                        if (newValue) API.mediaElement.attr("webkit-playsinline", "");
-                        else API.mediaElement.removeAttr("webkit-playsinline");
+                        if (newValue) {
+			    API.mediaElement.attr("webkit-playsinline", "");
+			    API.mediaElement.attr("playsinline", "");
+		        } else {
+			    API.mediaElement.removeAttr("webkit-playsinline");
+			    API.mediaElement.removeAttr("playsinline");
+			}
                     }
                 );
 
